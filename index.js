689a9694c67064cfdba4a89c0cf5c959eb728a5c
Filip Tammergård <filip@tammergard.se>
2022-03-30 08:03:33 +0200

    chore: clean up workflows

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = {
   branches: ["main"],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     "@semantic-release/npm",
+    "@semantic-release/release-notes-generator",
   ],
 }
